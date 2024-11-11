134c6674ee3ea4fd801dd456d4ed2e9735661d51
5-abdulsami <5abdulsami2004@gmail.com>
2024-11-11 19:01:18 +0500

    implemented search functionality

--- lib/SearchProduct/search_product.dart
+++ lib/SearchProduct/search_product.dart
@@ -8,19 +8,47 @@ class SearchProduct extends StatefulWidget {
 }
 
 class _SearchProductState extends State<SearchProduct> {
+  final searchController = TextEditingController();
+  String searchText = '';
+
+  updateSearchText(String text) {
+    setState(() {
+      searchText = text;
+    });
+  }
+
+  Widget _buildSearchBar() {
+    return Container(
+      child: TextField(
+        onChanged: (value) => updateSearchText(value),
+        controller: searchController,
+        autofocus: true,
+        decoration: InputDecoration(
+          hintText: "Search for products",
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Search Product Screen",
-          style: TextStyle(
-              fontSize: 30, fontFamily: 'Signatra', color: Colors.black),
+    return Container(
+      decoration: BoxDecoration(color: Colors.teal),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text(
+            "Search Product Screen",
+            style: TextStyle(
+                fontSize: 30, fontFamily: 'Signatra', color: Colors.black),
+          ),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(color: Colors.teal),
+          ),
+          actions: const [],
         ),
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(color: Colors.teal),
-        ),
-        actions: const [],
       ),
     );
   }
